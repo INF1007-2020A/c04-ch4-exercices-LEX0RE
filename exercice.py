64ac3c45632f2e7d719f945aac771ca842ad2ceb
LEX0RE <gelinas.alexandre.2001@gmail.com>
2020-09-15 23:13:24 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -29,6 +29,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     inword = False
+    inspace = False
     
     for x in string:
         if x == char:
