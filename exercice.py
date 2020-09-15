ea84bece5a2ad8ecd1834e64ecec2ae4333c6335
LEX0RE <gelinas.alexandre.2001@gmail.com>
2020-09-15 23:15:44 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -39,7 +39,7 @@ def get_number_of_words(sentence: str, word: str) -> int:
             if inspace == True:
                 pass
             else
-                
+                inspace = 
     return occurence
 
 
