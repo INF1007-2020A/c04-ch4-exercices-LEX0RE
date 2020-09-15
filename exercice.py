59af62c554582bafc087f8dfa9cefa130bf4da77
LEX0RE <gelinas.alexandre.2001@gmail.com>
2020-09-15 23:01:25 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -29,8 +29,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    get_number_of_char(sentence, )
-    pass
+    return get_number_of_char(sentence, ' ')
 
 
 def main() -> None:
