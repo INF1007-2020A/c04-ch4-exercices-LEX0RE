026c6f3ba44559a69fe293f71d3b0aa5a127d390
LEX0RE <gelinas.alexandre.2001@gmail.com>
2020-09-15 22:58:51 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,7 +21,8 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    for 
+    for x in string:
+        if x == char
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
