aec25f32abfde99921d22d57429e91cb116a2154
LEX0RE <gelinas.alexandre.2001@gmail.com>
2020-09-15 23:07:50 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,8 @@
 def is_even_len(string: str) -> bool:
     if (len(string) % 2):
         return 0
-    return !(len(string) % 2)
+    else:
+        return 1
 
 
 def remove_third_char(string: str) -> str:
